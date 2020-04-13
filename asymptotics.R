690885a26cb442fe72aa23f29942782f2c7e6e36
guillermohenrion <guillermo.henrion@gmail.com>
2020-04-12 23:34:02 -0300

    Update asymptotics.R

--- asymptotics.R
+++ asymptotics.R
@@ -3,13 +3,24 @@ pval<-seq(0.1,0.9,by=0.05)
 pval
 nosim<-1000
 coverage <- sapply(pval, function(p) {
-  phats<-rbinom(nosim, prob=p, size=n)/n
+  phats<-(rbinom(nosim, prob=p, size=n))/(n)
   ll<-phats-qnorm(0.975)*sqrt(phats*(1-phats)/n)
   ul<-phats+qnorm(0.975)*sqrt(phats*(1-phats)/n)
   mean(ll<p&ul>p)
 })
 
-plot(coverage, type='l')
+
+coverage2 <- sapply(pval, function(p) {
+  phats<-(rbinom(nosim, prob=p, size=n)+2)/(n+4)
+  ll<-phats-qnorm(0.975)*sqrt(phats*(1-phats)/n)
+  ul<-phats+qnorm(0.975)*sqrt(phats*(1-phats)/n)
+  mean(ll<p&ul>p)
+})
+
+plot(coverage, type='o', col='blue')
+plot(coverage2, type='o', col='green')
+
+
 
 
   
