e67bc5993d8be7fbe47d33dc79a750649e74e44a
guillermohenrion <guillermo.henrion@gmail.com>
2020-04-12 23:03:31 -0300

    Add files via upload

---
+++ asymptotics.R
@@ -0,0 +1,15 @@
+n<-20
+pval<-seq(0.1,0.9,by=0.05)
+pval
+nosim<-1000
+coverage <- sapply(pval, function(p) {
+  phats<-rbinom(nosim, prob=p, size=n)/n
+  ll<-phats-qnorm(0.975)*sqrt(phats*(1-phats)/n)
+  ul<-phats+qnorm(0.975)*sqrt(phats*(1-phats)/n)
+  mean(ll<p&ul>p)
+})
+
+plot(coverage, type='l')
+
+
+  
